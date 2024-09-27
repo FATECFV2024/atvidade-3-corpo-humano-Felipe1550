0048f8bb6ebe8bd52998b325a8b3f60122cce1d3
Felipe1550 <fesschueller@gmail.com>
2024-09-27 00:47:48 -0300

    alterando valores

--- src/test/CorpoHumanoTest.java
+++ src/test/CorpoHumanoTest.java
@@ -1,5 +1,5 @@
 package test;
-// CorpoHumanoTest.java
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -12,15 +12,15 @@ public class CorpoHumanoTest {
 
     @Before
     public void setUp() {
-        corpo = new CorpoHumano(70, 0.07, 1000, 1.75);
+        corpo = new CorpoHumano(70, 0.07f, 1000, 1.75f);
     }
 
     @Test
     public void testGetters() {
         assertEquals(70, corpo.getMassa(), 0.01);
-        assertEquals(0.07, corpo.getVolume(), 0.01);
+        assertEquals(0.07f, corpo.getVolume(), 0.01);
         assertEquals(1000, corpo.getDensidade(), 0.01);
-        assertEquals(1.75, corpo.getAltura(), 0.01);
+        assertEquals(1.75f, corpo.getAltura(), 0.01);
     }
 
     @Test
@@ -28,24 +28,24 @@ public class CorpoHumanoTest {
         corpo.setMassa(80);
         assertEquals(80, corpo.getMassa(), 0.01);
 
-        corpo.setVolume(0.08);
-        assertEquals(0.08, corpo.getVolume(), 0.01);
+        corpo.setVolume(0.08f);
+        assertEquals(0.08f, corpo.getVolume(), 0.01);
 
         corpo.setDensidade(1100);
         assertEquals(1100, corpo.getDensidade(), 0.01);
 
-        corpo.setAltura(1.80);
-        assertEquals(1.80, corpo.getAltura(), 0.01);
+        corpo.setAltura(1.80f);
+        assertEquals(1.80f, corpo.getAltura(), 0.01);
     }
 
     @Test
     public void testCalcularIMC() {
-        double expectedIMC = 70 / (1.75 * 1.75);
+        float expectedIMC = corpo.getMassa() / (corpo.getAltura() * corpo.getAltura());
         assertEquals(expectedIMC, corpo.calcularIMC(), 0.01);
 
         corpo.setMassa(80);
-        corpo.setAltura(1.80);
-        expectedIMC = 80 / (1.80 * 1.80);
+        corpo.setAltura(1.80f);
+        expectedIMC = 80 / (1.80f * 1.80f);
         assertEquals(expectedIMC, corpo.calcularIMC(), 0.01);
     }
 }
